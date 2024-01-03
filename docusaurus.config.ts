2fae9d570609925c8ec4a41308a21eaf02f7d1e6
sskyy <skyking_H@hotmail.com>
2024-01-03 12:45:50 +0800

    chore: change toc level

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -119,6 +119,10 @@ const config: Config = {
       theme: prismThemes.dracula,
       darkTheme: prismThemes.dracula,
     },
+    tableOfContents: {
+      minHeadingLevel: 2,
+      maxHeadingLevel: 5,
+    },
   } satisfies Preset.ThemeConfig,
   plugins:    [
     // ....
